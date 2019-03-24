a4452200848bebeba59c9aed9e6c1c65a6f2c590
Joshua Flanagan <joshuaflanagan@gmail.com>
2019-03-24 10:51:49 -0500

    Package linux binaries for gems with native extensions
    This assume the extensions, built for linux, already exist locally.
    
    If running on OSX, you can build the linux extensions using:
    
    ```
    docker run --rm -v "$PWD:/var/task" lambci/lambda:build-ruby2.5 bundle install --standalone --path vendor/bundle
    ```

--- index.js
+++ index.js
@@ -16,38 +16,49 @@ class PackageRubyBundlePlugin {
 
   beforePackage(){
     const gemRoot = "vendor/bundle/ruby/2.5.0"; //TODO: infer? configure?
+    const extensionDir = `${gemRoot}/extensions/x86_64-linux/2.5.0-static/`;
     const excludeGemTests = true; //TODO: make configurable
     const identifyGemsScript = `
-      gems = Bundler.definition.specs_for([:default]).reject{|s| s.name=='bundler'}
-      paths = gems.map(&:full_gem_path)
+      require 'json'
       root = ENV['GEM_HOME']
-      relative = paths.map{|p| p.split(root).last}.sort
-      puts relative`
+      gems = Bundler.definition.specs_for([:default]).reject{|s| s.name=='bundler'}
+      details = gems.map{|gem|
+        {
+          extensions: !!gem.extensions.any?,
+          name: gem.full_name,
+          path: gem.full_gem_path.split(root).last,
+        }
+      }
+      puts JSON.generate(details.sort_by{|x| x[:name]})
+    `
 
     this.serverless.service.package.exclude = ["**"]; // force whitelist
     this.serverless.service.package.include.push("vendor/bundle/bundler/**"); // bundler standalone files
 
     const gemFilePath = this.serverless.config.servicePath + "/Gemfile"
     const output = execSync(`BUNDLE_GEMFILE=${gemFilePath} bundle exec ruby`, {input: identifyGemsScript});
-    const gems = output.toString().trim().split("\n");
+    const gems = JSON.parse(output)
 
     if (gems.length < 10) {
-      this.serverless.cli.log(`Bundling gems: ${gems.map(x=>path.basename(x)).sort().join(" ")}`);
+      this.serverless.cli.log(`Bundling gems: ${gems.map(x=>x.name).join(" ")}`);
     } else {
       this.serverless.cli.log(`Bundling ${gems.length} gems`);
     }
 
     gems.forEach((gem) =>{
-      this.serverless.service.package.include.push(`${gemRoot}${gem}/**`);
+      this.serverless.service.package.include.push(`${gemRoot}${gem.path}/**`);
+      if (gem.extensions){
+        this.serverless.service.package.include.push(`${extensionDir}${gem.name}/**`);
+      }
 
       // includes that start with a ! are treated as excludes when evaluating,
       // but are ordered along with the includes. If these patterns were
       // specified as excludes, they would be evaluated first, and then the
       // includes on the gem paths would bring them back.
-      this.serverless.service.package.include.push(`!${gemRoot}${gem}/.git/**`);
+      this.serverless.service.package.include.push(`!${gemRoot}${gem.path}/.git/**`);
       if (excludeGemTests) {
-        this.serverless.service.package.include.push(`!${gemRoot}${gem}/test/**`);
-        this.serverless.service.package.include.push(`!${gemRoot}${gem}/spec/**`);
+        this.serverless.service.package.include.push(`!${gemRoot}${gem.path}/test/**`);
+        this.serverless.service.package.include.push(`!${gemRoot}${gem.path}/spec/**`);
       }
     });
   }
